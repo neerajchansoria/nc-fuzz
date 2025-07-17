934e2cd47519daafde3b4da4e48dbc0e5c19d789
Neeraj Chansoriya <neeraj.chansoria@gmail.com>
2025-07-17 04:24:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Installing dependencies..."
+echo "[*] Installing NC-FUZZ dependencies..."
+sudo apt update
+sudo apt install -y curl jq unzip git
+
+echo "[*] Installing Go tools..."
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/tomnomnom/assetfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+
+echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
+source ~/.bashrc
+echo "[✓] Installation complete."
